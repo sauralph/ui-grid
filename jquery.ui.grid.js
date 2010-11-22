25f7bcd33bf30ac78cb0705564c9580871b5b451
Sauralph <enuske@gmail.com>
2010-11-22 18:00:01 -0300

    Adding basic sorting functions Adding basic grid Logic (Retrievieng/changing data from cells) Adding Test cases

--- jquery.ui.grid.js
+++ jquery.ui.grid.js
@@ -2,9 +2,26 @@
 
 $.widget( "ui.grid", {
 	options:{
+		localSorting:false,
 		titleBar:true,
-		footerBar:true
-		
+		footerBar:true,
+		buttons:{
+			"< Previous":{text:false,icons:{primary:"ui-icon-triangle-1-w"}},
+			"Add":{icons:{primary:"ui-icon-plus"}},
+			"Remove":{icons:{primary:"ui-icon-trash"}},
+			"Refresh":{icons:{primary:"ui-icon-refresh"}},
+			"Next >":{text:false,icons:{primary:"ui-icon-triangle-1-e"}}
+		},
+		sort:{	
+			"default" : function(a,b){
+				return a.value - b.value;
+			},
+
+			"reverse" : function(a,b){
+				return b.value-a.value;
+			}
+
+		}	
 	},
 	_create:function(){
 		var self = this;
@@ -23,13 +40,15 @@ $.widget( "ui.grid", {
 		//build footer
 		var buttons = $("<div/>",{class:"ui-grid-paging ui-helper-clearfix"});
 		
-		buttons.append($("<a/>",{text:"< Previous"}).button({text:false,icons:{primary:"ui-icon-triangle-1-w"}}));
-		buttons.append($("<a/>",{text:"Add"}).button({icons:{primary:"ui-icon-plus"}}));
-		buttons.append($("<a/>",{text:"Remove"}).button({icons:{primary:"ui-icon-trash"}}));
-		buttons.append($("<a/>",{text:"Refresh"}).button({icons:{primary:"ui-icon-refresh"}}));
-		buttons.append($("<a/>",{text:"Next >"}).button({text:false,icons:{primary:"ui-icon-triangle-1-e"}}))
+		if(this.options.buttons){
+			for(name in this.options.buttons){
+				buttons.append($("<a/>",{text:name}).button(this.options.buttons[name]));
+			}
+			buttons.buttonset();	
+		}
 		
-		buttons.buttonset();
+
+		
 		
 		var results = $("<div/>",{class:"ui-grid-results"});
 		var footer  = $("<div/>",{
@@ -65,27 +84,109 @@ $.widget( "ui.grid", {
 		
 		//building menu
 		this.menu = $( "<ul/>" );
-		this.menu.append($("<li/>").append($("<a/>",{text:"Order Ascending"})));
-		this.menu.append($("<li/>").append($("<a/>",{text:"Order Descending"})));
-		this.menu.append($("<li/>").append($("<a/>",{text:"Filter by this field..."})));
-		this.menu.append($("<li/>").append($("<a/>",{text:"Group by this field..."})));
+		this.menu.append($("<li/>",{style:"cursor:pointer;"}).append($("<a/>",{data:{command:function(c){self.sort(c,"default")}},text:"Order Ascending"})));
+		this.menu.append($("<li/>",{style:"cursor:pointer;"}).append($("<a/>",{data:{command:function(c){self.sort(c,"reverse")}},text:"Order Descending"})));
+		this.menu.append($("<li/>",{style:"cursor:pointer;"}).append($("<a/>",{data:{command:function(c){alert(c+" Placeholder!")}},text:"Filter by this field..."})));
+		this.menu.append($("<li/>",{style:"cursor:pointer;"}).append($("<a/>",{data:{command:function(c){alert(c+" Placeholder!")}},text:"Group by this field..."})));
 		
-		this.menu.menu().appendTo("body").hide();
+		this.menu.menu({
+			selected:function(event,data){
+				//theres got to be a better way to do this...
+				fn = $("a#ui-active-menuitem").data("command");
+				column = self.menu.data("column");
+				try{
+					fn(column);
+				}finally{
+					self.menu.slideToggle("fast");	
+				}
+				
+			}
+		}).appendTo("body").hide();
+
 		
 		table.find("th a").click(function(){
 			var position = $(this).position();
 			self.menu.css({
 				position:'absolute',
 				top:position.top+$(this).outerHeight(),
-				left:position.left
+				left:position.left,
+				width:$(this).width
 				});
 			self.menu.slideToggle("fast");
+			self.menu.data("column",$(this).parent("th").index()+1);
+		});	
+	},
+	sort:function(column,fn){
+		switch(typeof fn){
+			case "string":
+				if(this.options.sort[fn]){
+					fn = this.options.sort[fn];
+				}else{
+					throw new Error("Sort function "+fn+" not defined in options");
+				}
+				break;
+			case "undefined":
+			default:
+				fn = this.options.sort.default;
+		}
+		
+		var reference = this.getColumnValues(column,function(e,i){
+			return {value:$(e).text(),row:$(e).parent("tr")}
 		});
+		//var original = reference;
+		reference.sort(fn);
+		
+		var tbody = this.element.find("tbody");
+		
+		for (var i = reference.length - 1; i >= 0; i--){
+			reference[i].row.prependTo(tbody);
+		};
 		
 		
+	},
+	
+	getColumn:function(column){
+		if(isNaN(column)||column===0){
+			column=1;
+		}
+		return this.element.find("tr td:nth-child("+column+")"); 
+	},
+	
+	getColumnValues:function(column,parser){
+		var set = this.getColumn(column);
 		
+		if(typeof parser ==="undefined"){
+			parser = function(input,index){
+				return $(input).text();
+			}
+		}
+		
+		return set.map(function(i,e){ return parser(e,i) }).get();
+		
+	},
+	
+	getCell:function(row,column){
+		return this.element.find("tr:nth-child("+row+") td:nth-child("+column+")"); 
+	},
+	
+	getCellValue:function(row,column,parser){
+		if(typeof parser ==="undefined"){
+			parser = function(input){
+				return input;
+			}
+		}
+		return parser(this.getCell(row,column).text()); 
+	},
+	
+	setCellValue:function(row,column,value){
+		return this.getCell(row,column).text(value);
+	},
+	
+	getRowCount:function(){
+		return this.element.find("tr:not(:has(th))").length; 
 		
 	}
+	
 });
 //core functions	
 	
